ba41abb849317cc836b8c7a92d803a5a2719996f
Yann L <130548077+ShowYL@users.noreply.github.com>
2025-02-03 01:36:42 +0100

    temp home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<main class="h-screen w-screen">
+    <div class="h-full w-full bg-no-repeat bg-cover bg-center flex flex-col items-center" style="background-image:url(/background-lock.jpg)">
+        <h1 class="text-center text-amber-100 mt-28 text-xl backdrop-blur-sm">Password Management</h1>
+        <button class="mt-auto mb-24 rounded border border-transparent hover:border-amber-100 p-1.5 text-amber-100 bg-slate-900 bg-opacity-60 hover:bg-opacity-80">Dive in</button>
+    </div>
+</main>
